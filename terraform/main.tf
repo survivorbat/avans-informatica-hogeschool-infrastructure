05ff028f2a45468fd0a14628ef7cc4b808ac3bf3
survivorbat <djbatcat@gmail.com>
2020-06-25 20:37:30 +0200

    master: Add ttl

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,7 +48,7 @@ resource "digitalocean_record" "student_subdomains" {
   domain   = digitalocean_domain.domain.name
   name     = "${each.key}.student"
   type     = "A"
-  ttl      = 16
+  ttl      = 32
   value    = module.student_vms[each.key].ipv4
 }
 
@@ -58,7 +58,7 @@ resource "digitalocean_record" "docent_subdomains" {
   domain   = digitalocean_domain.domain.name
   name     = each.value.subdomain
   type     = "A"
-  ttl      = 16
+  ttl      = 32
   value    = module.docent_vms[each.key].ipv4
 }
 
